6945534e1f5a9d93d6d3494047a5007c54e951bd
Isaac Ellmen <isaacellmen@gmail.com>
2018-10-17 16:54:19 -0400

    Talk to server

--- turbocat-client/src/App.js
+++ turbocat-client/src/App.js
@@ -1,20 +1,78 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.png';
 import './App.css';
 
 import { CartesianGrid, Label, Line, LineChart, ReferenceLine, Tooltip, XAxis, YAxis } from 'recharts';
 
+const apiUrl = 'http://localhost:5000'
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ratio: props.ratio,
+      results: [],
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateResults()
+    this.interval = setInterval(() => this.updateResults(), 10000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  handleChange(event) {
+    const ratio = event.target.value;
+    this.setRatio(ratio);
+    this.setState({ratio});
+  }
+
+  updateResults() {
+    fetch(`${apiUrl}/results`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            results: result
+          });
+        },
+        (error) => {
+          // this.setState({
+          //   isLoaded: true,
+          //   error
+          // });
+        }
+      )
+  }
+
+  setRatio(ratio) {
+    fetch(`${apiUrl}/set_ratio`, {method: 'POST', body: JSON.stringify({ratio})})
+      .then(res => res.json())
+      .then(
+        (result) => {
+          // this.setState({
+          //   results: result
+          // });
+          console.log(result);
+        },
+        (error) => {
+          // this.setState({
+          //   isLoaded: true,
+          //   error
+          // });
+        }
+      )
+  }
+
   render() {
-    const data = [
-      {name: '0m', percent: 50},
-      {name: '15m', percent: 60},
-      {name: '30m', percent: 65},
-      {name: '45m', percent: 60},
-      {name: '60m', percent: 50},
-      {name: '75m', percent: 45},
-      {name: '90m', percent: 50},
-    ];
+    const data = this.state.results;
     return (
       <div className="App">
         <header className="App-header">
@@ -22,9 +80,23 @@ class App extends Component {
         </header>
         <div className="main">
           <div className="chart">
-            <h1>Latest measurements</h1>
+            <form className="setRatio">
+              <div className="form-group">
+                <label htmlFor="formControlRange">
+                  <h2>Target population ratio ({this.state.ratio}%)</h2>
+                </label>
+                <input
+                  type="range"
+                  value={this.state.ratio}
+                  onChange={this.handleChange}
+                  className="form-control-range"
+                  id="formControlRange"
+                />
+              </div>
+            </form>
+            <h2>Latest measurements</h2>
             <LineChart width={800} height={300} margin={{ top: 0, right: 30, left: 30, bottom: 30 }} data={data}>
-              <ReferenceLine y={50} stroke="blue"/>
+              <ReferenceLine y={this.state.ratio} stroke="blue"/>
               <Line type="monotone" dataKey="percent" stroke="#483c6c" />
               <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
               <XAxis dataKey="name" padding={{right: 15}}>
@@ -41,5 +113,13 @@ class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  ratio: PropTypes.number,
+};
+
+App.defaultProps = {
+  ratio: 50,
+};
 
 export default App;
