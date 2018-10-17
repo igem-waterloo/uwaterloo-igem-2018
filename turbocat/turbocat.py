384990e4df6a7568d5485eb4120d5a5f4b2dd7d5
Isaac Ellmen <isaacellmen@gmail.com>
2018-10-17 16:55:17 -0400

    Mock server client talk

--- turbocat/turbocat.py
+++ turbocat/turbocat.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, json, jsonify, request
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def turbocat():
     return 'Hello, World!'
 
-# TODO: routes for setting desired ratio (POST)
-# TODO: routes for getting experimental, predicted ratios (GET)
+@app.route('/results')
+def results():
+    data = [
+      {'name': '0m', 'percent': 50},
+      {'name': '15m', 'percent': 60},
+      {'name': '30m', 'percent': 65},
+      {'name': '45m', 'percent': 60},
+      {'name': '60m', 'percent': 50},
+      {'name': '75m', 'percent': 45},
+      {'name': '90m', 'percent': 50},
+    ];
+    return jsonify(data)
+
+@app.route('/set_ratio', methods=['POST'])
+def set_ratio():
+    ratio = json.loads(request.data).get('ratio')
+
+    return ratio
