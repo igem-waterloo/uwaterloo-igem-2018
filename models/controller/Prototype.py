4ff1e74b8f4f0d5933b0d490c202d3242204f792
mastwood <mastwood101@gmail.com>
2018-10-17 22:25:19 -0400

    prototype code updates

--- models/controller/Prototype.py
+++ models/controller/Prototype.py
@@ -6,32 +6,33 @@ import matplotlib.pyplot as plt
 
 #----- Process Simulation Setup -----#
 
-p = GEKKO()
-p.time = [0,0.5]
+p = GEKKO(remote = True)
+p.time = np.linspace(0,0.5,0.01)
 p.dl = p.MV()
 p.beta = p.Param(value=0.05)
-p.pop = p.SV()
+p.pop = p.CV()
 p.g = p.CV()
-p.C = p.Param(value=1)
-
+p.C = p.Param(value = 1)
+p.t = p.Param(value = p.time)
 A = 20
 K = 300
-p.Equation(p.dl.dt() == (A+(K-A)/(1+20*p.exp(-t*p.beta)))*p.g)
-p.Equation(p.pop.dt() == p.C*p.dl.dt())
+p.Equation((A+(K-A)/(1+20*p.exp(-p.t*p.beta)))*p.g.dt() == p.dl)
+p.Equation(p.pop.dt() == p.C*p.dl)
 
-p.Options.IMODE = 4
+p.options.IMODE = 4
 
 #----- Moving Horizon Estimation Model Setup -----#
-
-mhe = GEKKO()
+mhe = GEKKO(remote = True)
+mhe.time = np.linspace(0,0.5,0.01)
 mhe.dl = mhe.MV() #output
 mhe.beta = mhe.FV(value = 0.01, lb = 0.0000001, ub = 0.5)
 mhe.C = mhe.FV(value = 1, lb = 0.001, ub = 5)
-mhe.g = mhe.SV() #measured variable
+mhe.g = mhe.CV() #measured variable
 mhe.pop = mhe.CV()
+mhe.t = mhe.Param(value = mhe.time)
 
-mhe.Equation(mhe.dl.dt() == (A+(K-A)/(1+20*mhe.exp(-t*0.05)))*mhe.g)
-mhe.Equation(mhe.pop.dt() == mhe.C*p.dl.dt())
+mhe.Equation(mhe.dl == (A+(K-A)/(1+20*mhe.exp(-mhe.t*0.05)))*mhe.g.dt())
+mhe.Equation(mhe.pop.dt() == mhe.C*p.dl)
 mhe.options.IMODE = 5
 mhe.options.EV_TYPE = 1
 mhe.options.DIAGLEVEL = 0
@@ -40,9 +41,9 @@ mhe.options.DIAGLEVEL = 0
 # STATUS = 1, optimizer can adjust
 mhe.dl.STATUS = 0
 mhe.beta.STATUS = 1
-mhe.g.STATUS = 1
 mhe.C.STATUS = 1
 mhe.pop.STATUS = 1
+mhe.g.STATUS = 1
 
 # FSTATUS = 0, no measurement
 # FSTATUS = 1, measurement used to update model
@@ -55,22 +56,23 @@ mhe.pop.FSTATUS = 1
 mhe.beta.DMAX = 0.01
 mhe.C.DMAX = 0.01
 
+mhe.pop.MEAS_GAP = 10
 mhe.g.MEAS_GAP = 0.01
 
 mhe.g.TR_INIT = 1
 
 #----- Model Predictive Control Setup -----#
-
-mpc = GEKKO()
-
+mpc = GEKKO(remote = True)
+mpc.time = np.linspace(0,0.5,0.01)
 mpc.dl = mpc.MV(lb = 0, ub = 1)
 mpc.beta = mpc.FV(value = 0.01, lb = 0.0000001, ub = 0.5)
 mpc.C = mpc.FV(value = 1, lb = 0.001, ub = 5)
-mpc.g = mpc.SV()
-mhe.pop = mhe.CV()
+mpc.g = mpc.CV()
+mpc.pop = mpc.CV()
+mpc.t = mpc.Param(value = mpc.time)
 
-mpc.Equation(mpc.dl.dt() == (A+(K-A)/(1+20*mpc.exp(-t*0.05)))*mpc.g)
-mpc.Equation(mpc.pop.dt() == mpc.C*p.dl.dt())
+mpc.Equation(mpc.dl == (A+(K-A)/(1+20*mpc.exp(-mpc.t*0.05)))*mpc.g.dt())
+mpc.Equation(mpc.pop.dt() == mpc.C*p.dl)
 
 mpc.dl.STATUS = 1
 mpc.beta.STATUS = 0
@@ -92,43 +94,45 @@ mpc.pop.SPHI = setpoint+0.1
 mpc.pop.SPLO = setpoint-0.1
 
 mpc.pop.TR_INIT = 0
-
+mpc.options.IMODE = 6
 
 #----- Simulation Loop for TurboCat Demo -----#
 
-population_meas = np.array([])
-population_esti = np.array([])
-g_meas = np.array([])
-g_esti = np.array([])
-C_esti = np.array([])
-beta_esti = np.array([])
-dl_cont = np.array([])
-setpoints = np.array([])
-
+population_meas = []
+population_esti = []
+g_meas = []
+g_esti = []
+C_esti = []
+beta_esti = []
+dl_cont = []
+setpoints = []
+plt.figure(figsize=(10,7))
+plt.ion()
+plt.show()
 def SimulationLoop(sp):
     setpoint = sp
     mpc.pop.SPHI = setpoint+0.1
     mpc.pop.SPLO = setpoint-0.1
-    setpoints = np.append(setpoints, [setpoint])
+    setpoints.append(setpoint)
 
     mpc.beta.MEAS = mhe.beta.NEWVAL
     mpc.C.MEAS = mhe.C.NEWVAL
 
-    if p.OPTIONS.SOLVESTATUS == 1 :
+    if p.options.SOLVESTATUS == 1 :
         mpc.g.MEAS = p.g.MODEL
         mpc.pop.MEAS = p.pop.MODEL
 
     mpc.solve(disp = True, debug = True)
-    dl_cont = np.append(dl_cont, [mpc.dl.NEWVAL])
+    dl_cont.append(mpc.dl.NEWVAL)
     p.dl.MEAS = dl_cont[dl_cont.size - 1]
 
     # import json
     # with open(mpc.path+'//results.json') as f:
-    #     results = json.load(f)
+    #      results = json.load(f)
 
     p.solve(disp = True, debug = True)
-    population_meas = np.append(population_meas, p.pop.MODEL + (random()*10 - 5))
-    g_meas = np.append(g_meas, p.g.MODEL + random()*0.1-0.05)
+    population_meas.append(p.pop.MODEL + (random()*10 - 5))
+    g_meas.append(p.g.MODEL + random()*0.1-0.05)
 
     mhe.dl.MEAS = dl_cont[dl_cont.size - 1]
     mhe.g.MEAS = g_meas[g_meas.size - 1]
@@ -136,19 +140,10 @@ def SimulationLoop(sp):
 
     mhe.solve(disp = True, debug = True)
 
-    population_esti = np.append(population_esti, [mhe.pop.MODEL])
-    g_esti = np.append(g_esti, [mhe.g.MODEL])
-    C_esti = np.append(C_esti, [mhe.C.NEWVAL])
-    beta_esti = np.append(beta_esti, [mhe.beta.NEWVAL])
-
-#----- TEST OF SIMULATION LOCALLY -----#
-i = 0
-plt.figure(figsize=(10,7))
-plt.ion()
-plt.show()
-while i < 21:
-    i=i+1
-    SimulationLoop(100)
+    population_esti.append(mhe.pop.MODEL)
+    g_esti.append(mhe.g.MODEL)
+    C_esti.append(mhe.C.NEWVAL)
+    beta_esti.append(mhe.beta.NEWVAL)
     plt.clf()
     plt.subplot(4,1,1)
     plt.plot(g_meas[0:i])
@@ -175,3 +170,9 @@ while i < 21:
     plt.legend('meas', 'pred', 'setpoint')
     plt.draw()
     plt.pause(0.05)
+
+#----- TEST OF SIMULATION LOCALLY -----#
+i = 0
+while i <= 21:
+    i=i+1
+    SimulationLoop(100)
